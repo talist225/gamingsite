a85ac3f4eba30dcfc9ccd57c288bef30dd497664
talist225 <109045845+talist225@users.noreply.github.com>
2022-09-04 01:38:32 +0300

    Add files via upload

--- js/components/cart.component.js
+++ js/components/cart.component.js
@@ -1,15 +1,23 @@
-const displayCart = (id) => {
-  let cartToDisplay = 0;
-  for (let i = 0; i < gameListArr.length; i++) {
-    if (gameListArr[i].id == id) {
-      cartToDisplay = i;
-      break;
-    }
-  }
-  document.getElementById("cartDivToDisplay").innerHTML = `
+var cart = document.getElementById("open-cart");
+
+// const displayCart = (id) => {
+//   let cartToDisplay = 0;
+//   for (let i = 0; i < gameListArr.length; i++) {
+//     if (gameListArr[i].id == id) {
+//       cartToDisplay = i;
+//       break;
+//     }
+//   }
+
+const displayCart = () => {
+  let cartToDisplay = document.getElementById("cartDivToDisplay");
+  cartToDisplay.innerHTML = "";
+  for (let gameListItem of gameListArr) {
+    cartToDisplay.innerHTML = `
   <div id="open-cart" class="cart">
   <span id="closeBtn" class="close">&times;</span>
 <div class="cart-content">
+<h2>Shopping Cart</h2>
   <div>
       <ul class="table">
       <li class="table-header">
@@ -18,32 +26,46 @@ const displayCart = (id) => {
         <div class="col col-3">Price</div>
         <div class="col col-4">Status</div>
       </li>
-      <li class="table-row">
-      <div class="col col-1">${gameListArr[cartToDisplay].dataItem}</div>
-      <div class="col col-2">${gameListArr[cartToDisplay].title}</div>
-      <div class="col col-3">$${gameListArr[cartToDisplay].price}</div>
-        <div class="col col-4"> <a href="#">Remove</a></div>
+      <li class="table-row" id="remove">
+      <div class="col col-1">${gameListItem.dataItem}</div>
+      <div class="col col-2">${gameListItem.title}</div>
+      <div class="col col-3">$${gameListItem.price}</div>
+        <div class="col col-4"> <a href="#" onclick="deleteItem()">Remove</a></div>
         
        
       </li>
-      <li class="table-row">
-      <div class="col col-1">${gameListArr[10].dataItem}</div>
-      <div class="col col-2">${gameListArr[10].title}</div>
-      <div class="col col-3">${gameListArr[10].price}</div>
-        <div class="col col-4"><a href="#">Remove</a></div>
+      <li class="table-row" id="remove">
+      <div class="col col-1">${gameListItem.dataItem}</div>
+      <div class="col col-2">${gameListItem.title}</div>
+      <div class="col col-3">${gameListItem.price}</div>
+        <div class="col col-4"><a href="#" onclick="deleteItem()">Remove</a></div>
       </li>
     </ul>
   </div>
   <span>
-  <p>
-  Total Price: $${gameListArr[cartToDisplay].price}</p>
+  <p id="totalPrice">
+  Total Price: $${gameListItem.price}</p>
   <div class="cardBtn">
   <a href="#">Buy Now</a>
   </span>
 </div>
 </div>
   `;
+  }
+
   document.getElementById("closeBtn").addEventListener("click", () => {
     document.getElementById("cartDivToDisplay").innerHTML = "";
   });
 };
+document.querySelector("#cart").addEventListener("click", () => {
+  if (document.querySelector("#cartDivToDisplay").innerHTML) {
+    document.querySelector("#cartDivToDisplay").innerHTML = "";
+  } else {
+    displayCart();
+  }
+});
+
+function deleteItem() {
+  const deleteItem = document.getElementById("remove");
+  deleteItem.remove(".table-row");
+}
